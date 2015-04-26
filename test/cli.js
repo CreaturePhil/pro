8e13cee39e3a24b96e69723b3614782bfc03930a
Phil <birkal@outlook.com>
2015-04-26 15:59:48 -0700

    Add core pro tests

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,9 @@
 var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
+var rimraf = require('rimraf');
 
+var pro = require('../lib/main');
 var set = require('../lib/set');
 
 describe('Setting user and files', function() {
@@ -25,5 +27,50 @@ describe('Setting user and files', function() {
         done();
       });
     });
+  });
+});
+
+describe('Getting repos and files', function() {
+  var alpha, index, Files;
+
+  before(function() {
+    alpha = path.join(__dirname, '../alpha');
+    index = path.join(__dirname, '../index.html');
+    Files = ['.gitignore', '.travis.yml', 'Gruntfile.js', 'Procfile', 'README.md', 'app', 'bin', 'config', 'package.json', 'server.js', 'test'];
+  });
+
+  after(function(done) {
+    rimraf(alpha, function(err) {
+      if (err) done(err);
+      fs.unlink(index, function(err) {
+        if (err) done(err);
+        done();
+      });
+    });
+  });
+
+  it('should get the repo to directory', function(done) {
+    pro('alpha', 'alpha', function() {
+      fs.exists(alpha, function(exists) {
+        assert(exists, true);
+        fs.readdir(alpha, function(err, files) {
+          if (err) done(err); 
+          assert.deepEqual(files.length, 11);
+          files.forEach(function(file, index) {
+            assert.deepEqual(file, Files[index]);
+          });
+          done();
+        });
+      });
+    });
+  });  
+
+  it('should get the file', function(done) {
+    pro('index.html', null, function() {
+      fs.exists(index, function(exists) {
+        assert(exists, true); 
+        done();
+      });
+    }); 
   });
 });

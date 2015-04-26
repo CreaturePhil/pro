93ffc0902dac7131b69db5cffaa589eba858df84
Phil <birkal@outlook.com>
2015-04-26 02:38:27 -0700

    Add setting user and file test

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,29 @@
 var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
-var pro = require('../lib/pro');
+
+var set = require('../lib/set');
 
 describe('Setting user and files', function() {
+  it('should have a user.txt file', function(done) {
+    set('user', 'creaturephil', function() {
+      fs.exists(path.join(__dirname, '../user.txt'), function(exists) {
+        assert(exists, true);
+        var user = fs.readFileSync(path.join(__dirname, '../user.txt'));
+        assert.deepEqual(String(user), 'creaturephil');
+        done();
+      });
+    });
+  });
+
+  it('should have a file.txt file', function(done) {
+    set('file', 'grid', function() {
+      fs.exists(path.join(__dirname, '../file.txt'), function(exists) {
+        assert(exists, true);
+        var file = fs.readFileSync(path.join(__dirname, '../file.txt'));
+        assert.deepEqual(String(file), 'grid');
+        done();
+      });
+    });
+  });
 });

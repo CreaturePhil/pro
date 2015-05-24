ec3add3b01a8f97e20d4cca82749fa7e87b5d01a
Phil <birkal@outlook.com>
2015-05-23 22:58:59 -0700

    Add db tests

--- test/cli.js
+++ test/cli.js
@@ -4,31 +4,6 @@ var path = require('path');
 var rimraf = require('rimraf');
 
 var pro = require('../lib/main');
-var set = require('../lib/set');
-
-describe('Setting user and files', function() {
-  it('should have a user.txt file', function(done) {
-    set('user', 'creaturephil', function() {
-      fs.exists(path.join(__dirname, '../user.txt'), function(exists) {
-        assert(exists, true);
-        var user = fs.readFileSync(path.join(__dirname, '../user.txt'));
-        assert.deepEqual(String(user), 'creaturephil');
-        done();
-      });
-    });
-  });
-
-  it('should have a file.txt file', function(done) {
-    set('file', 'grid', function() {
-      fs.exists(path.join(__dirname, '../file.txt'), function(exists) {
-        assert(exists, true);
-        var file = fs.readFileSync(path.join(__dirname, '../file.txt'));
-        assert.deepEqual(String(file), 'grid');
-        done();
-      });
-    });
-  });
-});
 
 describe('Getting repos and files', function() {
   //jscs:disable disallowMultipleVarDecl
---
+++ test/db.js
@@ -0,0 +1,63 @@
+var assert = require('assert');
+var db = require('../lib/db');
+var fs = require('fs');
+var path = require('path');
+var jsonfile = path.join(__dirname, '..', 'db.json');
+
+describe('db#set', function() {
+  it('should set user', function(done) {
+    db.set('user', 'CreaturePhil');
+    setTimeout(function() {
+      fs.readFile(jsonfile, 'utf-8', function(err, file) {
+        if (err) return done(err);
+        var json = JSON.parse(file);
+        assert.deepEqual(typeof json, 'object');
+        assert.deepEqual(typeof json.user, 'string');
+        assert.deepEqual(json.user, 'CreaturePhil');
+        done();
+      });
+    }, 100);
+  });
+
+
+  it('should set file', function(done) {
+    db.set('file', 'grid');
+    setTimeout(function() {
+      fs.readFile(jsonfile, 'utf-8', function(err, file) {
+        if (err) return done(err);
+        var json = JSON.parse(file);
+        assert.deepEqual(typeof json, 'object');
+        assert.deepEqual(typeof json.file, 'string');
+        assert.deepEqual(json.file, 'grid');
+        done();
+      });
+    }, 100);
+  });
+});
+
+describe('db#find', function() {
+  it('should get user', function(done) {
+    db.find('user', function(user) {
+      assert.deepEqual(typeof user, 'string');
+      assert.deepEqual(user, 'CreaturePhil');
+      done();
+    }); 
+  });
+
+  it('should get file', function(done) {
+    db.find('file', function(file) {
+      assert.deepEqual(typeof file, 'string');
+      assert.deepEqual(file, 'grid');
+      done();
+    }); 
+  });
+
+  it('should get the whole collection', function(done) {
+    db.find(function(collection) {
+      assert.deepEqual(typeof collection, 'object');
+      assert.deepEqual(collection.user, 'CreaturePhil');
+      assert.deepEqual(collection.file, 'grid');
+      done();
+    });
+  });
+});
